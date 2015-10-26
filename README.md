b40331f81a8c14154a04fd04783e403d3d8651f7
ackuser <mail@ackarim.com>
2015-10-26 11:49:44 +0000

    Fixing num of visible brands

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # angular-menu-buttons
-# angular-menu-buttons
--- app.js
+++ app.js
@@ -7,17 +7,43 @@ app.controller("BrandsCtrl", function($scope) {
     'Square','Stripe','Paypal','Apple Pay','Google Wallet'
   ];
 
-  $scope.itemFullList = '';
+
 
   $scope.brand1 = 'Paypal';
 
-  $scope.brand2 = ' ';
+  $scope.brand2 = '';
   $scope.enable2 = false;
-  $scope.brand3 = '  ';
+  $scope.brand3 = '';
   $scope.enable3 = false;
   $scope.num_visible_brands = 1;
 
+  $scope.variable1 = 'Volume of speech';
+  $scope.variable2 = '';
+  $scope.enable_var2 = false;
+  $scope.variable3 = '';
+  $scope.enable_var3 = false;
+  $scope.num_visible_vars = 1;
 
+
+  $scope.variable_titles = [
+    '','Volume of speech','Average influence','Proportion of top influencers','Proportion positive sentiment','Total interactions last 5 days','Awareness','Consideration','Sentiment','Usage','Volume of searches','Volume of searches for downloads'
+  ];
+
+  $scope.variable_speech = [
+    'Volume of speech','Average influence','Proportion of top influencers','Proportion positive sentiment'
+  ];
+
+  $scope.variable_stated = [
+    'Awareness','Consideration','Proportion of top influencers','Usage'
+  ];
+
+  $scope.variable_search = [
+    'Volume of searches','Volume of searches for downloads'
+  ];
+
+  $scope.variable_stated = [
+    'Total interactions last 5 days'
+  ];
 
 
   $scope.updateBrand = function ($event, num_brand ,name)
@@ -64,6 +90,29 @@ app.controller("BrandsCtrl", function($scope) {
                  };
 
 
+
+
+
+
+                 $scope.updateVariable = function ($event, num_var ,name)
+                 {
+                   switch(num_var) {
+                       case 1:
+                           $scope.variable1 = name;
+                           break;
+                       case 2:
+                           $scope.variable2 = name;
+                           break;
+                       case 3:
+                           $scope.variable3 = name;
+                           break;
+                   }
+                   //$event.stopPropagation();
+                 }
+
+                 $scope.notCurrentVariable = function(variable) {
+                      return variable !== $scope.variable1 && variable !== $scope.variable2  && variable !== $scope.variable3;
+                    };
 
 
             });
